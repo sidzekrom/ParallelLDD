1db04dc891d8c55b4a102e0a2d0912d71ddde617
Jacob Imola <jimola@ghc66.ghc.andrew.cmu.edu>
2017-04-30 15:43:45 -0400

    Added Makefile. Finished Miller-Peng-Xu (possible bugs). Added timing framework

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,94 @@
 #include <iostream>
 #include <vector>
 #include "sdd.h"
+#include <unistd.h>
+#include "CycleTimer.h"
+
+std::string helper_string = "Flags:\n"
+                       "v: verbose\n"
+                       "g: we will generate the graph. Takes in argument for type. Will prompt for size.\n"
+                       "    0: weakly connected cliques\n"
+                       "    1: user-inputted graph (TODO)\n"
+                       "f: enter a graph from a file (TODO)\n"
+                       "b: beta value. Default 0.1\n";
 
 void generate_clusters(int num_clusters, int cluster_size, graph &G){
-  G = graph(num_clusters*cluster_size, std::vector<int> (0));
-  for(int i = 0; i<G.size(); i+=cluster_size){
-    for(int j = 0; j<cluster_size; j++){
-      for(int k = j+1; k<cluster_size; k++){
-        G[i+j].push_back(i+k);
-        G[i+k].push_back(i+j);
-      }
+    G = graph(num_clusters*cluster_size, std::vector<int> (0));
+    for(int i = 0; i<G.size(); i+=cluster_size){
+        for(int j = 0; j<cluster_size; j++){
+            for(int k = j+1; k<cluster_size; k++){
+                G[i+j].push_back(i+k);
+                G[i+k].push_back(i+j);
+            }
+        }
+        if(i+cluster_size< G.size()){
+            G[i].push_back(i+cluster_size);
+            G[i+cluster_size].push_back(i);
+        }
     }
-    if(i+cluster_size< G.size()){
-      G[i].push_back(i+cluster_size);
-      G[i+cluster_size].push_back(i);
-    }
-
-  }
 }
-int main() {
+int main(int argc, char **argv) {
     /* parse arguments: this specifies which version of LDD to run */
     /* fill in parse arguments code here */
     /* end parse arguments */
-    std::cout << "0 for inputted graph\n1 for n weakly-connected cliques of size k\n";
-    int type; std::cin >> type;
+    int verbose = 0, type = -1, c;
+    double beta = 0.1;
+    char *f_name = NULL;
     graph graph_adj;
-    std::vector<int> clusters;
-    double beta;
-    if(type == 0){
-        /* creating graph from standard input right now, try to do something else */
+    while((c = getopt(argc, argv, "vg:b:")) != -1){
+        switch (c){
+            case 'v':
+                verbose = 1;
+                break;
+            case 'g':
+                type = atoi(optarg);
+                break;
+            case 'f':
+                f_name = optarg;
+                break;
+            case 'b':
+                beta = atof(optarg);
+                break;
+            case 'h':
+                std::cout << helper_string;
+                return 0;
+        }
+    }
+    if(type == -1){
+        std::cout << helper_string; return 0;
+    }
+    if(type == 1){
         int num_vertices;
         int num_edges;
-        std::cin >> num_vertices >> num_edges >> beta;
-    }else if(type == 1){
+        std::cout << "Enter number of vertices and edges: ";
+        std::cin >> num_vertices >> num_edges;
+    }else if(type == 0){
         int n, k;
-        std::cin >> n >> k >> beta;
+        std::cout << "Enter number of cliques and clique size: ";
+        std::cin >> n >> k;
         generate_clusters(n, k, graph_adj);
+    }else{
+        std::cout << "Invalid type\n"; return 0;
     }
-    sequentialLDD(graph_adj, clusters, beta);
-    std::cout << "Clusters:\n";
-    for(auto x : clusters)
-        std::cout << x << " ";
-    std::cout << "\n";
+    std::vector<int> clusters_seq, clusters_mpx;
+    double start = CycleTimer::currentSeconds();
+    sequentialLDD(graph_adj, clusters_seq, beta);
+    double mid = CycleTimer::currentSeconds();
+    millerPengXuLDD(graph_adj, clusters_mpx, beta);
+    double end = CycleTimer::currentSeconds();
+    std::cout << "Seq Clusters took: " << mid - start << " ns\n";
+    if(verbose){
+        std::cout << "Clusters:\n";
+        for(auto x : clusters_seq)
+            std::cout << x << " ";
+        std::cout << "\n\n";
+    }
+    std::cout << "Miller,Peng,Xu Clusters took: " << end - mid << " ns\n";
+    if(verbose){
+        std::cout << "Clusters:\n";
+        for(auto x : clusters_mpx)
+            std::cout << x << " ";
+        std::cout << "\n\n";
+    }
     return 0;
 }
--- src/sdd.cpp
+++ src/sdd.cpp
@@ -55,15 +55,12 @@ class frontier_elem{
     int cluster;
     double tie_breaker;
     frontier_elem(int v, int c, double t) : vtx(v), cluster(c), tie_breaker(t){}
-    /*
-    frontier_elem(){
-        vtx = 0; cluster = 0; tie_breaker = 0;    
-    };
-    */
+    frontier_elem(){ vtx = -1; cluster = 0; tie_breaker = 0; }
 };
 void millerPengXuLDD(graph &input_graph, std::vector<int> &clusters, double beta){
     clusters = std::vector<int> (input_graph.size(), -1);
     std::vector<double> start_times(input_graph.size());
+    std::vector<frontier_elem> tie_breakers (input_graph.size());
     std::random_device rd;
     std::mt19937 gen(rd());
     std::exponential_distribution<double> d(beta);
@@ -79,21 +76,50 @@ void millerPengXuLDD(graph &input_graph, std::vector<int> &clusters, double beta
         }
     }
     std::vector<frontier_elem> frontier;
+    std::vector<int> putoffs;
     for(int i = 0; i<start_times.size(); i++){
         start_times[i] = mx - start_times[i];
-        frontier.push_back(frontier_elem(i, i, start_times[i] % 1.0));
+        if(start_times[i] < 1){
+            clusters[i] = i;
+            frontier.push_back(frontier_elem(i, i, start_times[i]));
+        }else{
+            putoffs.push_back(i);
+        }
     }
-    int num_rounds = 0;
+    int num_rounds = 1;
     while(!frontier.empty()){
         std::vector<frontier_elem> next_frontier;
+        std::vector<int> next_putoffs;
         for(auto x : frontier){
-            if(start_times[x.vtx] > num_rounds+1){
-                next_frontier.push_back(x);
-            }else if(start_times[x.vtx] > num_rounds){
-                if(clusters[x.vtx] == -1){
-                    clusters[x.vtx] = x.cluster;
+            for(int v : input_graph[x.vtx]){
+                if(clusters[v] == -1){
+                    next_frontier.push_back(frontier_elem(v, x.cluster, x.tie_breaker));
                 }
             }
         }
+        //Sort out the new frontier
+        for(int x : putoffs){
+            if(start_times[x] < num_rounds+1)
+                next_frontier.push_back(frontier_elem(x, x, start_times[x] - num_rounds));
+            else
+                next_putoffs.push_back(x);
+        }
+        for(auto x : next_frontier){
+            if(tie_breakers[x.vtx].vtx == -1 || tie_breakers[x.vtx].tie_breaker > x.tie_breaker){
+                tie_breakers[x.vtx] = x;
+            }
+        }
+        frontier.clear();
+        putoffs.clear();
+        for(auto x : next_frontier){
+            if(clusters[x.vtx] == -1){
+                frontier.push_back(tie_breakers[x.vtx]);
+                clusters[x.vtx] = tie_breakers[x.vtx].cluster;
+            }
+        }
+        putoffs = next_putoffs;
+        next_frontier.clear();
+        next_putoffs.clear();
+        num_rounds++;
     }
 }
